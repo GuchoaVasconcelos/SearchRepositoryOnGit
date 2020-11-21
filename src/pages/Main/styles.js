b474e03e1492625d0c8db395f9b56e64c159228d
GuchoaVasconcelos <gabriel.uchoa.vasconcelos@gmail.com>
2020-11-21 19:37:37 -0300

    feat: add list to styled components

--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -1,4 +1,4 @@
-import styled, {keyframes} from "styled-components";
+import styled, { keyframes } from "styled-components";
 
 export const Container = styled.div`
   max-width: 700px;
@@ -34,7 +34,7 @@ export const Form = styled.form`
   }
 `;
 
-const rotate = keyframes `
+const rotate = keyframes`
 from {
   transform: rotate(0deg);
 }
@@ -44,7 +44,10 @@ to {
 }
 `;
 
-export const SubmitButton = styled.button.attrs(props => ({ type: "submit", disabled: props.loading }))`
+export const SubmitButton = styled.button.attrs((props) => ({
+  type: "submit",
+  disabled: props.loading,
+}))`
   background: #7159c1;
   border: 0;
   padding: 0 15px;
@@ -54,12 +57,33 @@ export const SubmitButton = styled.button.attrs(props => ({ type: "submit", disa
   justify-content: center;
   align-items: center;
 
-  &[disabled]{
+  &[disabled] {
     cursor: not-allowed;
     opacity: 0.6;
-  svg{
-    animation: ${rotate} 2s linear infinite;
-      }
+    svg {
+      animation: ${rotate} 2s linear infinite;
+    }
   }
+`;
 
+export const List = styled.ul`
+  list-style: none;
+  margin-top: 30px;
+
+  li {
+    padding: 15px 0;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-content: center;
+
+    & + li {
+      border-top: 1px solid #eee;
+    }
+
+    a {
+      color: #7159c1;
+      text-decoration: none;
+    }
+  }
 `;
