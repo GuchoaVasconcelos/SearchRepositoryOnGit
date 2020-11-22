6450313fc2e7b7d5dc4b5f4946eb3ed9d7ee9426
GuchoaVasconcelos <gabriel.uchoa.vasconcelos@gmail.com>
2020-11-21 23:49:52 -0300

    feat: add proptypes

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,11 +1,48 @@
-import React from "react";
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import api from "../../services/api";
 
 // import { Container } from './styles';
 
-// eslint-disable-next-line react/prop-types
-function Repository({ match }) {
-  // eslint-disable-next-line react/prop-types
-  return <h1>Repository: {decodeURIComponent(match.params.repository)} </h1>;
-}
+class Repository extends Component {
+  static PropTypes = {
+    match: PropTypes.shape({
+      params: PropTypes.shape({
+        repository: PropTypes.string,
+      }).isRequired,
+    }).isRequired,
+  };
+  state = {
+    repository: {},
+    issues: [],
+    loading: true,
+  };
 
+  async componentDidMount() {
+    const { match } = this.props;
+
+    const repoName = decodeURIComponent(match.params.repository);
+
+    const [repositoryGet, issues] = await Promise.all([
+      api.get(`/repos/${repoName}`),
+      api.get(`/repos/${repoName}/issues`, {
+        params: {
+          state: "open",
+          per_page: 5,
+        },
+      }),
+    ]);
+
+    this.setState({
+      loading: false,
+      repository: repositoryGet.data,
+      issues: issues.data,
+    });
+  }
+
+  render() {
+    const { repository, issues, loading } = this.state;
+    return <h1>Repository</h1>;
+  }
+}
 export default Repository;
